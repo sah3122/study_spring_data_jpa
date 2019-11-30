2ea3794ef2c3802f27e350a6b03a912557b6ac47
dongchul <sah3122@naver.com>
2019-11-30 17:14:25 +0900

    ADD Project Setting

---
+++ src/main/java/me/study/springdatajpa/JpaRunner.java
@@ -0,0 +1,29 @@
+package me.study.springdatajpa;
+
+import org.hibernate.Session;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Component
+@Transactional
+public class JpaRunner implements ApplicationRunner {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        Account account = new Account();
+        account.setUsername("dongchul");
+        account.setPassword("jpa");
+
+        Session session = entityManager.unwrap(Session.class); // hibernate의 가장 핵심적인 객체
+        session.save(account);
+        //entityManager.persist(account);
+    }
+}
