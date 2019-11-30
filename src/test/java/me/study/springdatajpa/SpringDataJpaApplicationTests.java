7c7ba45263cc4779d94d6dcf983e46da15c343c3
dongchul <sah3122@naver.com>
2019-11-30 16:46:26 +0900

    init

---
+++ src/test/java/me/study/springdatajpa/SpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.study.springdatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
